537f07111e28589745628b5db0ac4fa0a9156a09
PRABHAT SINGH <107301804+Prabhat1225@users.noreply.github.com>
2023-01-30 16:58:20 +0000

    map commit

---
+++ 205Isomorphic Strings.py
@@ -0,0 +1,20 @@
+# Given two strings s and t, determine if they are isomorphic.
+# Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+# All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+# Example 1:
+# Input: s = "egg", t = "add"
+# Output: true
+# Example 2:
+# Input: s = "foo", t = "bar"
+# Output: false
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        map1,map2={},{}
+        for i in range(len(s)):
+            c1,c2=s[i],t[i]
+            if((c1 in map1 and map1[c1]!=c2)or (c2 in map2 and map2[c2]!=c1)):
+                return False
+            map1[c1]=c2
+            map2[c2]=c1
+        return True        
